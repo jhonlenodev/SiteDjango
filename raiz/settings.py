3540f3f7ae952a5126abc5fed8cc63198e1bc46c
Mask <planosmask@gmail.com>
2021-06-25 05:51:29 -0300

    Login e cadastro de usuario

--- raiz/settings.py
+++ raiz/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    #Apps for 3
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     # My Apps
     'blog',
 ]
@@ -128,3 +133,18 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Django_allauth
+
+AUTHENTICATION_BACKENDS = [
+  
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+ 
+]
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
